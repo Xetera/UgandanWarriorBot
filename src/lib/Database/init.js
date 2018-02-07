3b916c46c7d02a63e295ec12f7273a0dc8a4e068
Ali <mythologyali@gmail.com>
2018-02-07 11:46:18 -0800

    mlab login setup

--- src/lib/Database/init.js
+++ src/lib/Database/init.js
@@ -5,12 +5,22 @@ const debug = require('../../Development/Debug');
 exports.login = function(){
     let date = new Date();
     if (settings.developmentMode)
+    {
         mongoose.connect('mongodb://localhost/telegram').then(() => {
             debug.info('Connected to MongoDB [[localhost]] in %o.', new Date() - date + ' milliseconds');
         }).catch(err => {
             debug.error('Problem connecting to MongoDB.');
             console.error(err);
         });
-    else
-        mongoose.connect(/* replace with heroku database for development*/);
+    }
+    else {
+        mongoose.connect(`mongodb://${process.env.MLAB_USER}:${process.env.MLAB_PASS}@ds123718.mlab.com:23718/heroku_810nbf98`)
+            .then(() => {
+                debug.info('Connected to MongoDB [[localhost]] in %o.', new Date() - date + ' milliseconds');
+            }).catch(err => {
+                debug.error('Problem connecting to MongoDB.');
+            console.error(err);
+        });
+    }
+
 };
