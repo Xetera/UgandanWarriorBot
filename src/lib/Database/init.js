0b9008302f60b2691f0b98de116b156b22d74a72
Ali <mythologyali@gmail.com>
2018-02-07 11:46:45 -0800

    init fix

--- src/lib/Database/init.js
+++ src/lib/Database/init.js
@@ -16,7 +16,7 @@ exports.login = function(){
     else {
         mongoose.connect(`mongodb://${process.env.MLAB_USER}:${process.env.MLAB_PASS}@ds123718.mlab.com:23718/heroku_810nbf98`)
             .then(() => {
-                debug.info('Connected to MongoDB [[localhost]] in %o.', new Date() - date + ' milliseconds');
+                debug.info('Connected to MongoDB [[cloud]] in %o.', new Date() - date + ' milliseconds');
             }).catch(err => {
                 debug.error('Problem connecting to MongoDB.');
             console.error(err);
